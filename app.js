0d7e9550fbce0ed489beffbc34944a5b033f3bfa
pasynkov <a@pasa.me>
2014-02-25 17:24:24 +0400

    route controller run method

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+console.log('\n\n\n\n\n\n');
+
 var vakoo = require('vakoo')();
 
 vakoo.listen();
---
+++ controllers/main.js
@@ -0,0 +1,10 @@
+var main = function(){
+
+    this.index = function(){
+        this.send({aza:'olo'});
+    }
+
+    return this;
+}
+
+module.exports = main;
