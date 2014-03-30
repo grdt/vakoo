393376fa645d9ada29db14a8e2f50ab6c56cff0b
pasynkov <a@pasa.me>
2014-03-30 21:40:50 +0400

    model core methods

--- components/main/models/page.js
+++ components/main/models/page.js
@@ -2,11 +2,26 @@ var Page = function(){
 
     this.COLLECTION_NAME = 'pages';
 
+    this._id = '';
 
+    this.alias = '';
 
-    this.alias = function(alias){
+    this.teaser = '';
+
+    this.content = '';
+
+    this.status = '';
+
+    this.added = new Date();
+
+    this.published = new Date();
+
+    this.last_modifed = new Date();
+
+    this.byAlias = function(alias,callback){
         this.where({alias:alias});
-        this.find();
+        this.findOne(callback);
+        return this;
     }
 
     return this;
--- system/database/drivers/mongodb_model.js
+++ system/database/drivers/mongodb_model.js
@@ -1,15 +1,128 @@
+var _ = require('underscore');
+
 var Model = function(){
 
+    this._where = {};
+
+    this._sort = {};
+
+    this._reservedKeys = ['COLLECTION_NAME','_collection'];
+
     this.where = function(params){
-        console.log(this.COLLECTION_NAME);
-        console.log('where',params);
+        for(key in params){
+            if(this.hasOwnProperty(key)){
+                this._where[key] = params[key];
+            }
+        }
     }
 
     this.find = function(){
 
     }
 
-    
+    this.findOne = function(callback){
+
+        var _this = this;
+
+        this.collection().findOne(this._where,function(err,item){
+            if(err){
+                console.log(err);
+            }else{
+                if(item){
+                    for(key in item){
+                        if(_this.hasOwnProperty(key)){
+                            _this[key] = item[key];
+                        }
+                    }
+
+                    if(typeof callback == "function"){
+                        callback(_this);
+                    }
+                }else{
+                    console.log('not found');
+                }
+            }
+        });
+    }
+
+    this.save = function(callback){
+        if(this._id){
+            this.update(callback);
+        }else{
+            this.insert(callback);
+        }
+    }
+
+    this.insert = function(callback){
+
+        var _this = this;
+
+        this.collection().insert(this.clean(['_id']),function(err,items){
+            if(err){
+                console.log(err);
+            }else{
+                if(items.length == 1){
+                    for(key in items[0]){
+                        if(_this.hasOwnProperty(key)){
+                            _this[key] = items[0][key];
+                        }
+                    }
+
+                    if(typeof callback == "function"){
+                        callback(_this);
+                    }
+
+                }else{
+                    //todo collection return
+                }
+            }
+        });
+
+        return this;
+    }
+
+    this.update = function(){
+        
+    }
+
+    this.clean = function(without){
+
+        //todo: refactor cleaner
+
+        var keys = _.keys(this);
+
+        for(key in this._reservedKeys){
+            keys = _.without(keys,this._reservedKeys[key]);
+        }
+
+        if(typeof without == "string"){
+            keys = _.without(keys,without);
+        }else{
+            if(typeof without == "object"){
+                for(key in without){
+                    keys = _.without(keys,without[key]);
+                }
+            }
+        }
+
+        var funcs = _.functions(this);
+
+        for(func in funcs){
+            keys = _.without(keys, funcs[func]);
+        }
+
+        var object = _.pick(this,keys);
+
+        return object;
+    }
+
+    this.collection = function(){
+        if(!!this._collection)
+            return this._collection;
+
+        this._collection = this.interface.collection(this.COLLECTION_NAME);
+        return this._collection;
+    }
 
     return this;
 }
