1c05f3b714f70d1db19394ab858cf07b42cbc80e
pasynkov <a@pasa.me>
2014-10-22 09:26:46 +0400

    forever restart

--- up.sh
+++ up.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#cd /var/www/luxy.sexy && git pull && npm restart
-cd /var/www/luxy.sexy && git pull && npm run pm2-restart
+cd /var/www/luxy.sexy && git pull && npm restart
+#cd /var/www/luxy.sexy && git pull && npm run pm2-restart
