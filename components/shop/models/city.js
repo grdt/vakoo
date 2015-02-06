d70b1b17b9f951873e0cc6e958a3787d676f8a63
pasynkov <a@pasa.me>
2015-02-06 20:37:46 +0400

    new cities structure

--- components/shop/models/city.js
+++ components/shop/models/city.js
@@ -35,8 +35,9 @@ var City = function(){
 				"in":this.title_in,
 				from:this.title_from
 			},
-			code:this.data.postal_code,
-			region: (this.data.region_type == 'г') ? '' : this.data.region + ' ' + (this.data.region_type_full || '')
+//			code:this.data.postal_code,
+//			region: (this.data.region_type == 'г') ? '' : this.data.region + ' ' + (this.data.region_type_full || '')
+			region: this.region
 		};
 		return object;
 	}
@@ -59,26 +60,9 @@ var City = function(){
 
 
 	this.ip2long = function(IP) {
-		var i = 0;
-
-		IP = IP.match(
-			/^([1-9]\d*|0[0-7]*|0x[\da-f]+)(?:\.([1-9]\d*|0[0-7]*|0x[\da-f]+))?(?:\.([1-9]\d*|0[0-7]*|0x[\da-f]+))?(?:\.([1-9]\d*|0[0-7]*|0x[\da-f]+))?$/i
-		);
-		if (!IP) {
-			return false;
-		}
-		IP[0] = 0;
-		for (i = 1; i < 5; i += 1) {
-			IP[0] += !! ((IP[i] || '')
-				.length);
-			IP[i] = parseInt(IP[i]) || 0;
-		}
-		IP.push(256, 256, 256, 256);
-		IP[4 + IP[0]] *= Math.pow(256, 4 - IP[0]);
-		if (IP[1] >= IP[5] || IP[2] >= IP[6] || IP[3] >= IP[7] || IP[4] >= IP[8]) {
-			return false;
-		}
-		return IP[1] * (IP[0] === 1 || 16777216) + IP[2] * (IP[0] <= 2 || 65536) + IP[3] * (IP[0] <= 3 || 256) + IP[4] * 1;
+        var ipA = IP.split(".");
+        var block = ((parseInt(ipA[0]) * 256 * 256 * 256) + (parseInt(ipA[1]) * 256 * 256) + (parseInt(ipA[2]) * 256) + parseInt(ipA[3]));
+        return block;
 	}
 
 	return this;
