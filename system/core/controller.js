48d9e711e880e98438f7c1b448c4c308759903ab
pasynkov <a@pasa.me>
2015-02-20 21:01:40 +0400

    Hide bot logs

--- system/core/controller.js
+++ system/core/controller.js
@@ -186,7 +186,7 @@ var CoreController = function(query){
                 if(err){
                     console.error("Mongo err", err)
                 }else{
-                    console.log("Bot detected `", bot, "`remove session is ok")
+//                    console.log("Bot detected `", bot, "`remove session is ok")
                 }
             })
         }
